eac7809ff1d8d0b70a9f225346560ed82a11f81f
zongyuan_n <627235655@qq.com>
2020-03-09 10:25:07 +0800

    up 0309

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ning-ui-blog
+
 前台博客系统基于 react，后台管理系统基于 vue，UI 基于自造框架 ning-ui。
+
+# 启动本地服务
+
+## 启动后台管理项目
+
+```
+cd admin
+npm run serve
+```
+
+## 启动前台博客系统
+
+```
+npm run dev
+```
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,21 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './Footer.scss';
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer className="ning-row">
-                <div className="last-content">
-                    <span>Designed & Powerd by zongyuan.ning</span>
-                    <span>Copyright© 2019 - 2019 n顾盼神飞</span>
-                    <span>蜀 ICP 备 19016370 号</span>
-                </div>
-            </footer>
-        )
-    }
+	render() {
+		return (
+			<footer className="ning-row">
+				<div className="last-content">
+					<span>Designed & Powerd by zongyuan.ning</span>
+					<span>Copyright© 2019 - 2020 n顾盼神飞</span>
+					<a href="http://beian.miit.gov.cn/state/outPortal/loginPortal.action;jsessionid=48i9Kb-v42k6wCOR5dGGgtnZfGpHfZUxtuNlB8SzndfA1bG4FhXc!-738760798">
+						蜀 ICP 备 19016370 号
+					</a>
+				</div>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
