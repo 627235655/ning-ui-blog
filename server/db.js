93e855a6b0bfd5bd936a255e3b4ecd69bde0fd41
zongyuan_n <627235655@qq.com>
2020-04-15 11:00:32 +0800

    up 0415

--- server/db.js
+++ server/db.js
@@ -1,25 +1,37 @@
-var mongoose = require('mongoose')
-mongoose.Promise = require('bluebird')
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+var { mongodb_connect_str } = require('./alioss'); // 这里不方便暴露自己的信息， 配置可看以下注释
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', {
-	useNewUrlParser: true
-}, (err, res) => {
-	if (err) {
-		console.log(err)
+// mongoose.connect('mongodb://127.0.0.1:27017/test', {
+// 	useNewUrlParser: true
+// }, (err, res) => {
+// 	if (err) {
+// 		console.log(err)
+// 	}
+// })
+
+mongoose.connect(
+	mongodb_connect_str,
+	{
+		useNewUrlParser: true,
+	},
+	(err, res) => {
+		if (err) {
+			console.log(err);
+		}
 	}
-})
-
+);
 
 var navListSchema = new mongoose.Schema({
 	id: String,
 	text: String,
-})
+});
 
 var tagListSchema = new mongoose.Schema({
 	id: String,
 	tagName: String,
 	articleNum: Number,
-})
+});
 
 var ArticleListSchema = new mongoose.Schema({
 	id: String,
@@ -36,12 +48,12 @@ var ArticleListSchema = new mongoose.Schema({
 	commentCount: Number,
 	createDate: Date,
 	updateDate: Date,
-})
+});
 
 var UserSchema = new mongoose.Schema({
 	username: String,
 	password: String,
-})
+});
 
 var CommentListSchema = new mongoose.Schema({
 	id: String,
@@ -57,8 +69,7 @@ var CommentListSchema = new mongoose.Schema({
 	subCommentList: Array,
 	likeCount: Number,
 	createDate: Date,
-})
-
+});
 
 var Models = {
 	NavList: mongoose.model('NavList', navListSchema),
@@ -66,6 +77,6 @@ var Models = {
 	ArticleList: mongoose.model('ArticleList', ArticleListSchema),
 	User: mongoose.model('User', UserSchema),
 	CommentList: mongoose.model('CommentList', CommentListSchema),
-}
+};
 
-module.exports = Models
+module.exports = Models;
